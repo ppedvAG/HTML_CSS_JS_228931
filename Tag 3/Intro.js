0dad9ec4b4c6726aa6144425adbb159dce1c0c24
PhilippLibowicz <83985701+PhilippLibowicz@users.noreply.github.com>
2023-09-21 08:46:46 +0200

    JavaScript
    Einführung in JavaScript + Oberflächlich Objekte, Windows, Math durchgenommen

---
+++ Tag 3/Intro.js
@@ -0,0 +1,77 @@
+// Ich bin ein Einzeiliger Kommentar
+console.log("Hallo Welt!");
+console.error("Da lief was schief");
+
+/*
+Mehrzeiliger Kommentar
+*/
+
+// Variablen
+
+let firstName = "Max";
+
+var Zahl = 5;
+
+const lastName = "Mustermann";
+
+// Wie gebe ich diese Variablen in der Konsole aus?
+console.log("Mein Name ist: " + firstName);
+
+console.log("Hallo " + firstName + " " + lastName);
+console.log(Zahl);
+
+// Datentypen + Operatoren
+var Zahl1 = 10;
+var Zahl2 = 20;
+var Ergebnis = Zahl1 * Zahl2;
+
+// Ausgabe auf der Seite
+document.write("Das Ergebnis ist: " + Ergebnis);
+
+// Arithmetischen Operatoren:
+// + Plus-Operator
+// - Minues-Operator
+// * Mal-Operator
+// / Geteilt-Operator
+// % Modulo-Operator => Wie "Geteilt" bloß mit Rest
+// ++ Inkrement-Operator => Schleifen
+// -- Dekrement-Operator => Schleifen
+// ** Exponential-Operator
+
+// Assignment-Operator
+// zahl = zahl * 4      => zahl *= 4
+// Kurzschreibweise
+
+// Inkrement ++
+var neueZahl = 0;
+neueZahl++;
+document.write("<br> Neue Zahl: " + neueZahl);
+
+// Dekrement
+neueZahl--;
+document.write("<br> Neue Zahl dekrementiert: " + neueZahl);
+
+// Boolean
+// true/false
+
+let wahr = true;
+let falsch = false;
+// Wird meistens für Vergleiche und if-Blöcke genutzt
+
+// NULL
+// Ist die absichtliche Zuweisung keines Wertes
+// Bedeutet die Abwesenheit von Daten
+let nichts = null;
+
+// typeof
+var alter = 18;
+document.write("<br>");
+document.write(typeof(alter));
+document.write("<br>");
+
+// Definiert zwei Variablen vom Type "Number"
+// Führe eine beliebige Arithemtische Operation mit den beiden Variablen aus
+// Weise das Ergebnis einer Konstanten zu
+// 1x Konsolenausgaben & 1x Auf der Seite ausgeben
+
+
